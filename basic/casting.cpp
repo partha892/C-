75718e1b4d4f38db9898e5d68d3f1626dc5ca194
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-11-21 17:17:30 +0530

    c++

---
+++ basic/casting.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+int main(){
+    
+    float chaiTemperacher = 67.65;
+    char temp = chaiTemperacher;
+     string tea = "masala \t chai";
+    cout<<temp<<endl;
+    cout<<tea<<endl;
+    
+
+
+   return 0;
+}
---
+++ basic/userInput.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int userNumber;
+cout<<"enter your number \n";
+cin>>userNumber;
+cout<<"your user input number is :"<<userNumber;
+
+    return 0;
+}
