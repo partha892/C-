94d6f3fef9b9e561bad0c3f2685e723d16507fa2
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-11-21 16:42:18 +0530

    c++

---
+++ basic/primitiveDataType.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int chaiOrderNumber = 1275003567;
+    float chaiTemperacher = 99.98755555;
+    double chaiPrice = 99.89925;
+    char chaiGreat = 'a';
+    bool isready = -231;
+   unsigned stock = -20;
+   long bigData = 234895689.67;
+
+    cout<<stock<<endl;
+    cout<<chaiOrderNumber<<endl;
+  
+
+    cout<<chaiTemperacher<<endl;
+    cout<<isready<<endl;
+     cout<<chaiPrice<<endl;
+       cout<<bigData<<endl;
+
+
+    return 0;
+}
